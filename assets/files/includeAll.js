702f995d50ede8da3da256ed2061b76e41e4e3bc
kolkatajubi <kolkatachamps@jubi.ai>
2019-12-19 20:01:55 +0530

    NLP browserify

--- assets/files/includeAll.js
+++ assets/files/includeAll.js
@@ -2,7 +2,8 @@ document.write(`
 <link href="https://fonts.googleapis.com/css?family=Muli:700&display=swap" rel="stylesheet" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://pixie.jubi.ai/videoConversationTrue/static/files/moduleRunnerLite.js"></script>
-<script src="https://pixie.jubi.ai/videoConversationTrue/static/files/actions.js"></script>
 <link rel="stylesheet" href="https://pixie.jubi.ai/videoConversationTrue/static/css/button.css" />
 <link rel="stylesheet" href="https://pixie.jubi.ai/videoConversationTrue/static/css/text.css" />
 `);
+
+// <script src="https://pixie.jubi.ai/videoConversationTrue/static/files/actions.js"></script>
--- assets/files/moduleRunner.js
+++ assets/files/moduleRunner.js
@@ -310,8 +310,53 @@ $(document).ready(() => {
       window.open(button.firstElementChild.getAttribute("value"));
     }
   });
+  //===================================================
+
+  $("#mic-icon").click(function(event) {
+    event.preventDefault();
+    // console.log("started");
+    myVideo.pause();
+    moduleRunner.blurBackground();
+    recognition.start();
+    $("#mic-icon").hide();
+    $("#mic-listening").show();
+  });
+  $("#mic-listening").click(function(event) {
+    event.preventDefault();
+    // console.log("stopped");
+    recognition.stop();
+    $("#mic-icon").show();
+    $("#mic-listening").hide();
+  });
+  //===================================================
 });
 
+//===================================================
+
+window.SpeechRecognition =
+  window.webkitSpeechRecognition || window.SpeechRecognition;
+let recognition = new window.SpeechRecognition();
+recognition.interimResults = true;
+recognition.maxAlternatives = 3;
+recognition.continuous = true;
+recognition.onresult = event => {
+  let interimTranscript = "";
+  for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+    let transcript = event.results[i][0].transcript;
+    if (event.results[i].isFinal) {
+      moduleRunner.NLP(transcript);
+      console.log("speech -> ", transcript);
+      recognition.stop();
+      $("#mic-listening").hide();
+      $("#mic-icon").show();
+    } else {
+      interimTranscript += transcript;
+      // console.log("listening", interimTranscript);
+    }
+  }
+};
+
+//===================================================
 var getSiblings = function(elem) {
   // Setup siblings array and get the first sibling
   var siblings = [];
